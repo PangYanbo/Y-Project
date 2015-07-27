ace150bef7d3e205dc64c5e4d006dc5514a78106
takayabe0505 <yabe0505@iis.u-tokyo.ac.jp>
2015-07-27 18:33:47 +0900

    2nd commit

--- src/testfiles/ID_LogCounter.java
+++ src/testfiles/ID_LogCounter.java
@@ -13,52 +13,69 @@ import java.util.HashMap;
 public class ID_LogCounter {
 
 	public static void main(String args[]) throws NumberFormatException, ParseException, IOException{
-		IdCounter(args[0],args[1]);
+		IdCounter(args[0],args[1],args[2]);
+		System.out.println("finished "+ args[0]);
 	}
-	
-//	public static void main(String args[]) throws NumberFormatException, ParseException, IOException{
-//		String in = "c:/users/yabetaka/desktop/test.txt";
-//		String out = "c:/users/yabetaka/desktop/testres.txt";
-//		IdCounter(in,out);
-//	}
 
-	public static void IdCounter(String in, String out){
+	//	public static void main(String args[]) throws NumberFormatException, ParseException, IOException{
+	//		String in = "c:/users/yabetaka/desktop/test.txt";
+	//		String out = "c:/users/yabetaka/desktop/testres.txt";
+	//		IdCounter(in,out);
+	//	}
+
+	public static void IdCounter(String in, String out, String matomeres){
 		int counter = 0;
 		int errorLine = 0;
+		int samelog = 0;
 		HashMap<String, Integer> id_count = new HashMap<String,Integer>();
 		try{
 			BufferedReader br = new BufferedReader(new FileReader(new File(in)));
 			BufferedWriter bw = new BufferedWriter(new FileWriter(new File(out)));
+			BufferedWriter bw2 = new BufferedWriter(new FileWriter(new File(matomeres),true));
 			String line = br.readLine();
+			String prevline = null;
 			while ((line=br.readLine())!=null){
-				String[] tokens = line.split("\t");
-				if(tokens.length>1){
-					String id = tokens[1];
-					if(id_count.containsKey(id)){
-						int count = id_count.get(id) + 1;
-						id_count.put(id, count);
+				if(SameLogCheck(line,prevline)==true){
+					String[] tokens = line.split("\t");
+					if(tokens.length>1){
+						String id = tokens[0];
+						if(!id.equals("null")){
+							if(id_count.containsKey(id)){
+								int count = id_count.get(id) + 1;
+								id_count.put(id, count);
+							}
+							else{
+								id_count.put(id, 1);
+							}
+							counter++;
+							//						if(counter % 1 ==0){
+							//							System.out.println("#done " + counter);
+							//						}
+						}
 					}
 					else{
-						id_count.put(id, 1);
-					}
-					counter++;
-					if(counter % 1 ==0){
-						System.out.println("#done " + counter);
+						errorLine++;
 					}
 				}
 				else{
-					errorLine++;
+					samelog++;
 				}
+				prevline = line;
 			}
-			System.out.println("done " + counter + " lines, and " + errorLine +" were error lines.");
+			System.out.println("done " + counter + " lines, and " + errorLine +" were error lines, and "+samelog+" were samelogs.");
 			System.out.println("done sorting out, will start writing them out...");
 
+			int bigusers = 0;
 			for(String str : id_count.keySet()){
-				if(id_count.get(str)>0){
-					bw.write(str + "\t" + id_count.get(str));
+				if(id_count.get(str)>10){
+					bw.write(str + "," + id_count.get(str));
 					bw.newLine();
+					bigusers++;
 				}
 			}
+			bw2.write(in + ":" + counter + "," + errorLine + "," + samelog + "," + bigusers);
+			bw2.newLine();
+			bw2.close();
 			br.close();
 			bw.close();
 		}
@@ -67,6 +84,15 @@ public class ID_LogCounter {
 		}
 		catch(IOException xxx) {
 			System.out.println(xxx);
+		}
+	}
+
+	public static boolean SameLogCheck(String line, String prevline){
+		if(line.equals(prevline)){
+			return false;
+		}
+		else{
+			return true;
 		}
 	}
 }
--- src/testfiles/hello1.java
+++ src/testfiles/hello1.java
@@ -1,37 +1,33 @@
 package testfiles;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.text.ParseException;
 
 public class hello1 {
 	public static void main(String args[]) throws NumberFormatException, ParseException, IOException{
-		IdCounter(args[0],args[1]);
+		IdCounter(args[0]);
 	}
 
-	public static void IdCounter(String in, String out){
+	public static void IdCounter(String in){
 		int counter = 0;
 		try{
 			BufferedReader br = new BufferedReader(new FileReader(new File(in)));
-			BufferedWriter bw = new BufferedWriter(new FileWriter(new File(out)));
 			String line = null;
 			while ((line=br.readLine())!=null){
-				bw.write(line);
-				bw.newLine();
-				counter++;
-				if(counter == 10){
-					break;
+				String[] tokens = line.split("\t");
+				if(tokens.length>1){
+					String id = tokens[0];
+					if(!id.equals("null")){
+						counter++;
+					}
 				}
 			}
-			System.out.println("done sorting out, will start putting them in order...");
-
+			System.out.println("null:" + counter);
 			br.close();
-			bw.close();
 		}
 		catch(FileNotFoundException xx) {
 			System.out.println("File not found 5");
