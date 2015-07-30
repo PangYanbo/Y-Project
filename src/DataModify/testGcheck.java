8bc03bf79663a1b63f01a85a559faad8070afc6e
takayabe0505 <yabe0505@iis.u-tokyo.ac.jp>
2015-07-30 10:10:36 +0900

    update

--- src/DataModify/testGcheck.java
+++ src/DataModify/testGcheck.java
@@ -11,21 +11,15 @@ import java.util.List;
 import jp.ac.ut.csis.pflow.geom.GeometryChecker;
 import jp.ac.ut.csis.pflow.geom.LonLat;
 
-public class AreaChecker {
+public class testGcheck {
 
-	/*
-	 * params 
-	 * args [0] : all data file
-	 * args [1] : result data file 
-	 * CHECK : shape file of area
-	 * 
-	 */
-	
 	public static void main(String args[]) throws IOException{
-		WriteonlyGcheckedLogs(args[0],args[1]);
+		File in = new File("c:/users/yabetaka/desktop/test.txt");
+		File out = new File("c:/users/yabetaka/desktop/testres.txt");
+		WriteonlyGcheckedLogs(in,out);
 	}
 	
-	static File shapedir = new File("/home/c-tyabe/Data/jpnshp");
+	static File shapedir = new File("c:/users/yabetaka/desktop/jpnshp");
 	static GeometryChecker gchecker = new GeometryChecker(shapedir);
 	
 	public static boolean AreaOverlap(LonLat point){
@@ -34,14 +28,15 @@ public class AreaChecker {
 			return false;
 		}
 		else{
+			System.out.println(zonecodeList.get(0));
 			return true;
 		}
 	}
 	
-	public static void WriteonlyGcheckedLogs(String args, String args2) throws IOException{
+	public static void WriteonlyGcheckedLogs(File args, File args2) throws IOException{
 		int counter = 0;
-		BufferedReader br = new BufferedReader(new FileReader(new File(args)));
-		BufferedWriter bw = new BufferedWriter(new FileWriter(new File(args2)));
+		BufferedReader br = new BufferedReader(new FileReader(args));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(args2));
 		String line = null;
 		while((line = br.readLine())!=null){
 			String[] tokens = line.split("\t");
@@ -52,7 +47,7 @@ public class AreaChecker {
 				bw.write(line);
 				bw.newLine();
 				counter++;
-				if(counter%100000==0){
+				if(counter%1==0){
 					System.out.println("#done : " + counter);
 				}
 			}
