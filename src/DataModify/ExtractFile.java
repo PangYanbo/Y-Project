4413dc20f83a2728d1489725b20816957f679b66
takayabe0505 <yabe0505@iis.u-tokyo.ac.jp>
2015-08-10 16:58:41 +0900

    update

--- src/DataModify/ExtractFile.java
+++ src/DataModify/ExtractFile.java
@@ -3,8 +3,12 @@ package DataModify;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.zip.GZIPInputStream;
 
 import org.apache.tools.tar.TarEntry;
@@ -12,23 +16,46 @@ import org.apache.tools.tar.TarInputStream;
 
 public class ExtractFile {
 
+	public static void main(String args[]) throws IOException{
+		uncompress(Paths.get(args[0]));
+	}
+	
 	public static void extract(String infile) throws IOException{
 		FileInputStream fis = new FileInputStream(infile);
 		TarInputStream tin  = new TarInputStream(new GZIPInputStream(fis));
 		TarEntry tarEnt = tin.getNextEntry();
-	    while (tarEnt != null) {
-	      String name = tarEnt.getName();
-	      int size = (int)tarEnt.getSize();
-	      System.out.println(String.format("%s:: size= %d byte",name,size));
-	      ByteArrayOutputStream bos = new ByteArrayOutputStream(size);
-	      tin.copyEntryContents(bos);
-	      byte[] data = bos.toByteArray();
-	      DataInputStream dis = new DataInputStream(new ByteArrayInputStream(data));
-	      
-	      dis.close();
-	      tarEnt = tin.getNextEntry();
-	    }
-	    tin.close();
+		while (tarEnt != null) {
+			String name = tarEnt.getName();
+			int size = (int)tarEnt.getSize();
+			System.out.println(String.format("%s:: size= %d byte",name,size));
+			ByteArrayOutputStream bos = new ByteArrayOutputStream(size);
+			tin.copyEntryContents(bos);
+			byte[] data = bos.toByteArray();
+			DataInputStream dis = new DataInputStream(new ByteArrayInputStream(data));
+
+			dis.close();
+			tarEnt = tin.getNextEntry();
+		}
+		tin.close();
 	}
-	
+
+	public static void uncompress(Path path) throws IOException {
+
+		if(!path.toString().endsWith(".tar.gz"))
+			throw new Error("extension must be tar.gz.");
+
+		TarInputStream tin = new TarInputStream(new GZIPInputStream(new FileInputStream(path.toFile())));
+
+		for(TarEntry tarEnt = tin.getNextEntry(); tarEnt != null; tarEnt = tin.getNextEntry()) {
+			if(tarEnt.isDirectory()){
+				new File(tarEnt.getName()).mkdir();
+			}
+			else {
+				FileOutputStream fos = new FileOutputStream(new File("/home/c-tyabe/Data/"+tarEnt.getName()));
+				tin.copyEntryContents(fos);
+			}
+		}
+		tin.close();
+	}
+
 }
