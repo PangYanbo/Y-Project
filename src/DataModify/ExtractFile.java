52239f41b709da4b0fc0b782dbb5b820cd249e32
takayabe0505 <yabe0505@iis.u-tokyo.ac.jp>
2015-08-24 10:17:19 +0900

    uncompress

--- src/DataModify/ExtractFile.java
+++ src/DataModify/ExtractFile.java
@@ -20,7 +20,7 @@ public class ExtractFile {
 	public static void main(String args[]) throws IOException{
 		uncompress(Paths.get(args[0]));
 	}
-	
+
 	public static void extract(String infile) throws IOException{
 		FileInputStream fis = new FileInputStream(infile);
 		TarInputStream tin  = new TarInputStream(new GZIPInputStream(fis));
@@ -40,50 +40,31 @@ public class ExtractFile {
 		tin.close();
 	}
 
-	public static void uncompress(Path path){
+	public static void uncompress(Path path) throws IOException{
 
 		if(!path.toString().endsWith(".tar.gz"))
 			throw new Error("extension must be tar.gz.");
 
-		TarInputStream tin = null;
-		try {
-			tin = new TarInputStream(new GZIPInputStream(new FileInputStream(path.toFile())));
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+		TarInputStream tin;
 
-		try {
-			for(TarEntry tarEnt = tin.getNextEntry(); tarEnt != null; tarEnt = tin.getNextEntry()) {
-				if(tarEnt.isDirectory()){
-					new File(tarEnt.getName()).mkdir();
-				}
-				else {
-					FileOutputStream fos = null;
-					try {
-						fos = new FileOutputStream(new File("/home/c-tyabe/Data/"+tarEnt.getName()+".csv"));
-					} catch (FileNotFoundException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-					try {
-						tin.copyEntryContents(fos);
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
+		tin = new TarInputStream(new GZIPInputStream(new FileInputStream(path.toFile())));
+		for(TarEntry tarEnt = tin.getNextEntry(); tarEnt != null; tarEnt = tin.getNextEntry()) {
+			if(tarEnt.isDirectory()){
+				new File(tarEnt.getName()).mkdir();
 			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			else {
+				FileOutputStream fos;
+				fos = new FileOutputStream(new File("/home/c-tyabe/Data/"+tarEnt.getName()+".csv"));
+				try {
+					tin.copyEntryContents(fos);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
 		}
-		try {
-			tin.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		tin.close();
 	}
 
 }
