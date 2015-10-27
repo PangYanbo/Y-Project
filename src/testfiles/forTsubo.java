74fe90bd4aba7b86f9d0a86c2d77d40039167b52
takayabe0505 <yabe0505@iis.u-tokyo.ac.jp>
2015-10-27 12:51:35 +0900

    sadad

--- src/testfiles/forTsubo.java
+++ src/testfiles/forTsubo.java
@@ -1,15 +1,19 @@
 package testfiles;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 
 import DataModify.ExtractFile;
+import IDExtract.ID_Extract_Tools;
 import MainRuns.FilePaths;
 
 public class forTsubo {
+
+	public static final String GPSdeeppath = "/home/c-tyabe/Data/grid/0/tmp/ktsubouc/gps_";
 
 	public static void main(String args[]) throws IOException{
 
@@ -23,23 +27,33 @@ public class forTsubo {
 		for(String ymd : subjects){
 			ExtractFile.extractfromcommand(ymd); System.out.println("#done uncompressing "+ymd);
 			String unzippedfile = FilePaths.deephomepath(ymd);
-			getres(unzippedfile);
+			getres(unzippedfile, ymd);
 		}
 	}
-	
-	public static void getres(String file) throws IOException{
+
+	public static void getres(String file, String ymd) throws IOException{
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		String line = null;
+		String prevline = null;
 		int count = 0;
 		HashSet<String> set = new HashSet<String>();
 		while((line=br.readLine())!= null){
-			String[] tokens = line.split("\t");
-			String id = tokens[1];
-			set.add(id);
-			count++;
+			if(ID_Extract_Tools.SameLogCheck(line,prevline)==true){
+				String[] tokens = line.split("\t");
+				if(tokens.length>=5){
+					if(!tokens[4].equals("null")){
+						String id = tokens[1];
+						set.add(id);
+						count++;
+					}
+				}
+				prevline = line;
+			}
 		}
 		System.out.println("number of lines: " +count + ", number of IDs:"+ set.size());
 		br.close();
+		File i = new File(GPSdeeppath+ymd+".csv");
+		i.delete();
 	}
 
 }
