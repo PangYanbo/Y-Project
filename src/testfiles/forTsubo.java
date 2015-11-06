1ba85e583b9ceabd2eec9a485871c61c57900aa3
takayabe0505 <yabe0505@iis.u-tokyo.ac.jp>
2015-11-06 13:47:41 +0900

    sadasda

--- src/testfiles/forTsubo.java
+++ src/testfiles/forTsubo.java
@@ -7,30 +7,33 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 
-import DataModify.ExtractFile;
 import IDExtract.ID_Extract_Tools;
-import MainRuns.FilePaths;
 
 public class forTsubo {
 
 	public static final String GPSdeeppath = "/home/c-tyabe/Data/grid/0/tmp/ktsubouc/gps_";
 
-	public static void main(String args[]) throws IOException{
-
-		File out = new File("/home/c-tyabe/Data/dayslogs_results.csv");
-
-		ArrayList<String> subjects = getdates();
-		//		for(int i = 0; i<subjects.size(); i++){
-		//			System.out.println(subjects.get(i));
-		//		}
-
-		for(String ymd : subjects){
-			ExtractFile.extractfromcommand(ymd); System.out.println("#done uncompressing "+ymd);
-			String unzippedfile = FilePaths.deephomepath(ymd);
-			getres(unzippedfile, ymd, out);
-		}
+//	public static void main(String args[]) throws IOException{
+//
+//		File out = new File("/home/c-tyabe/Data/dayslogs_results.csv");
+//
+//		ArrayList<String> subjects = getdates();
+//		//		for(int i = 0; i<subjects.size(); i++){
+//		//			System.out.println(subjects.get(i));
+//		//		}
+//
+//		for(String ymd : subjects){
+//			ExtractFile.extractfromcommand(ymd); System.out.println("#done uncompressing "+ymd);
+//			String unzippedfile = FilePaths.deephomepath(ymd);
+//			getres(unzippedfile, ymd, out);
+//		}
+//	}
+	
+	public static void main(String[] args){
+		
 	}
 
 	public static ArrayList<String> getdates(){
@@ -57,6 +60,34 @@ public class forTsubo {
 		String prevline = null;
 		int count = 0;
 		HashSet<String> set = new HashSet<String>();
+		while((line=br.readLine())!= null){
+			if(ID_Extract_Tools.SameLogCheck(line,prevline)==true){
+				String[] tokens = line.split("\t");
+				if(tokens.length>=5){
+					if(!tokens[4].equals("null")){
+						String id = tokens[0];
+						set.add(id);
+						count++;
+					}
+				}
+				prevline = line;
+			}
+		}
+		bw.write(ymd+","+count+","+set.size());
+		bw.newLine();
+		br.close();
+		bw.close();
+		File i = new File(GPSdeeppath+ymd+".csv");
+		i.delete();
+	}
+	
+	public static void getloghisto(String file, String ymd, File out) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(out,true));
+		String line = null;
+		String prevline = null;
+		int count = 0;
+		HashMap<String,ArrayList<String>> set = new HashMap<String,ArrayList<String>>();
 		while((line=br.readLine())!= null){
 			if(ID_Extract_Tools.SameLogCheck(line,prevline)==true){
 				String[] tokens = line.split("\t");
