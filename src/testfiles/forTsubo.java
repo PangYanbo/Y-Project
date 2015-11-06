b2effbd3d2c0bb0fa4fd36a2a6305e7d566ad061
takayabe0505 <yabe0505@iis.u-tokyo.ac.jp>
2015-11-06 13:57:30 +0900

    dsada

--- src/testfiles/forTsubo.java
+++ src/testfiles/forTsubo.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 
 import DataModify.ExtractFile;
@@ -40,7 +41,7 @@ public class forTsubo {
 	public static ArrayList<String> getdates(){
 		ArrayList<String> subjects = new ArrayList<String>();
 		String year = "2014";
-		for(int i = 10; i<=12; i++){
+		for(int i = 11; i<=12; i++){
 			for(int j = 1; j<=30; j++){
 				subjects.add(year+String.valueOf(i)+String.format("%02d", j));
 			}
@@ -82,31 +83,38 @@ public class forTsubo {
 		i.delete();
 	}
 
-	//	public static void getloghisto(String file, String ymd, File out) throws IOException{
-	//		BufferedReader br = new BufferedReader(new FileReader(file));
-	//		BufferedWriter bw = new BufferedWriter(new FileWriter(out,true));
-	//		String line = null;
-	//		String prevline = null;
-	//		int count = 0;
-	//		HashMap<String,ArrayList<String>> set = new HashMap<String,ArrayList<String>>();
-	//		while((line=br.readLine())!= null){
-	//			if(ID_Extract_Tools.SameLogCheck(line,prevline)==true){
-	//				String[] tokens = line.split("\t");
-	//				if(tokens.length>=5){
-	//					if(!tokens[4].equals("null")){
-	//						String id = tokens[0];
-	//						set.add(id);
-	//						count++;
-	//					}
-	//				}
-	//				prevline = line;
-	//			}
-	//		}
-	//		bw.write(ymd+","+count+","+set.size());
-	//		br.close();
-	//		bw.close();
-	//		File i = new File(GPSdeeppath+ymd+".csv");
-	//		i.delete();
-	//	}
+	public static void getloghisto(String file, String ymd, File out) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(out,true));
+		String line = null;
+		String prevline = null;
+		HashMap<String,Integer> set = new HashMap<String,Integer>();
+		while((line=br.readLine())!= null){
+			if(ID_Extract_Tools.SameLogCheck(line,prevline)==true){
+				String[] tokens = line.split("\t");
+				if(tokens.length>=5){
+					if(!tokens[4].equals("null")){
+						String id = tokens[0];
+						if(set.containsKey(id)){
+							int count = set.get(id) + 1;
+							set.put(id, count);
+						}
+						else{
+							set.put(id, 1);
+						}
+					}
+				}
+				prevline = line;
+			}
+		}
+		for(String ids : set.keySet()){
+			bw.write(ids+","+set.get(ids));
+			bw.newLine();
+		}
+		br.close();
+		bw.close();
+		File i = new File(GPSdeeppath+ymd+".csv");
+		i.delete();
+	}
 
 }
