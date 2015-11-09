ee6a809bc5501a07974c707369c794a4d9668879
takayabe0505 <yabe0505@iis.u-tokyo.ac.jp>
2015-11-09 10:22:08 +0900

    sadsad

--- src/MachineLearningAnalyse/MLDataDivider.java
+++ src/MachineLearningAnalyse/MLDataDivider.java
@@ -14,11 +14,12 @@ public class MLDataDivider {
 	public static void main(String args[]) throws IOException{
 
 		File in = new File("/home/c-tyabe/Data/MLResults_rain8_irr/"+args[0]+"_diff_ML_cleaned.csv");
-		File out1 = new File("/home/c-tyabe/Data/MLResults_rain8_irr/"+args[0]+"_diff_ML_cleaned_norfast.csv");
-		File out2 = new File("/home/c-tyabe/Data/MLResults_rain8_irr/"+args[0]+"_diff_ML_cleaned_norslow.csv");
-		
-		makeMLFile(in,out1,"-1");
-		makeMLFile(in,out2,"1");
+//		File out1 = new File("/home/c-tyabe/Data/MLResults_rain8_irr/"+args[0]+"_diff_ML_cleaned_norfast.csv");
+//		File out2 = new File("/home/c-tyabe/Data/MLResults_rain8_irr/"+args[0]+"_diff_ML_cleaned_norslow.csv");
+//
+//		makeMLFile(in,out1,"-1");
+//		makeMLFile(in,out2,"1");
+		count(in);
 	}
 
 	public static HashMap<String,ArrayList<String>> makeMLFile(File in, File out, String notthis) throws IOException{
@@ -37,6 +38,31 @@ public class MLDataDivider {
 		br.close();		
 		bw.close();
 		return res;
+	}
+
+	public static void count(File in) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(in));
+		String line = null;
+		int count1 = 0;
+		int count0 = 0;
+		int counti = 0;
+		int all = 0;
+		while((line=br.readLine())!= null){
+			String[] tokens = line.split(" ");
+			String val = tokens[0];
+			if(val.equals("1")){
+				count1++;
+			}
+			else if(val.equals("0")){
+				count0++;
+			}
+			else{
+				counti++;
+			}
+			all++;
+		}
+		br.close();		
+		System.out.println((double)count1/(double)all + "," + (double)count0/(double)all + "," + (double)counti/(double)all);
 	}
 
 }
