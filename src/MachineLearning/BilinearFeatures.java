d64a3d1c40a45e334f183b71558dee61ff830f6b
takayabe0505 <yabe0505@iis.u-tokyo.ac.jp>
2015-11-17 14:23:12 +0900

    sdadsad

--- src/MachineLearning/BilinearFeatures.java
+++ src/MachineLearning/BilinearFeatures.java
@@ -14,19 +14,19 @@ public class BilinearFeatures {
 	 * level x landprice             4000XX ok
 	 * level x distance              5000XX ok
 	 * time  x distance              6000XX ok
- 	 * 
+	 * 
 	 */
-	
+
 	/*
 	 * 3 digits: normal features
 	 * 5 digits: area codes
 	 * 6 digits: for bilinear codes
 	 * 
 	 */
-	
+
 	public static String bilinearline(String level, String time, String distance, LonLat homep, LonLat offp, 
 			HashMap<String, String> popmap, HashMap<LonLat, String> pricemap){
-				
+
 		String levelhpop  = "1000"+level(level)+citypop(homep,popmap);
 		String levelopop  = "2000"+level(level)+citypop(offp,popmap);
 		String leveltime  = "3000"+level(level)+time(time);
@@ -36,7 +36,7 @@ public class BilinearFeatures {
 		String res = " "+levelhpop+":1 "+levelopop+":1 "+leveltime+":1 "+levelprice+":1 "+leveldis+":1 "+timedis+":1";		
 		return res;
 	}
-	
+
 	public static String time(String time){
 		Double timerange = Double.parseDouble(time);
 		if(timerange<6){return "1";}
@@ -45,7 +45,7 @@ public class BilinearFeatures {
 		else if ((timerange>=16)&&(timerange<20)){return "4";}
 		else{return "5";}
 	}
-	
+
 	public static String level(String level){
 		if(level.equals("1")){return "1";}
 		else if(level.equals("2")){return "2";}
@@ -53,34 +53,49 @@ public class BilinearFeatures {
 		else if(level.equals("4")){return "4";}
 		else{return "5";}
 	}
-	
+
 	public static String distance(String distance){
 		Double dis = Double.parseDouble(distance);
-		if(dis<0.01){return "1";}
-		else if ((dis>=0.01)&&(dis<0.05)){return "2";}
-		else if ((dis>=0.05)&&(dis<0.25)){return "3";}
-		else if ((dis>=0.25)&&(dis<0.40)){return "4";}
-		else{return "5";}
+		if(dis.equals("null")){
+			return "1";
+		}
+		else{
+			if(dis<0.01){return "1";}
+			else if ((dis>=0.01)&&(dis<0.05)){return "2";}
+			else if ((dis>=0.05)&&(dis<0.25)){return "3";}
+			else if ((dis>=0.25)&&(dis<0.40)){return "4";}
+			else{return "5";}
+		}
 	}
-	
+
 	public static String landprice(LonLat point, HashMap<LonLat, String> pricemap){
 		String price = GetLandPrice.getnearestprice(pricemap,point);
-		Double priced = Double.parseDouble(price);
-		if(priced<0.0015){return "1";}
-		else if ((priced>=0.0015)&&(priced<0.003)){return "2";}
-		else if ((priced>=0.003)&&(priced<0.01)){return "3";}
-		else if ((priced>=0.01)&&(priced<0.05)){return "4";}
-		else{return "5";}
+		if(price.equals("null")){
+			return "1";
+		}
+		else{
+			Double priced = Double.parseDouble(price);
+			if(priced<0.0015){return "1";}
+			else if ((priced>=0.0015)&&(priced<0.003)){return "2";}
+			else if ((priced>=0.003)&&(priced<0.01)){return "3";}
+			else if ((priced>=0.01)&&(priced<0.05)){return "4";}
+			else{return "5";}
+		}
 	}
-	
+
 	public static String citypop(LonLat point, HashMap<String, String> popmap){
 		String nowm  = new Mesh(3, point.getLon(),point.getLat()).getCode();
 		String pop = popmap.get(nowm);
-		Double poprange = Double.parseDouble(pop);
-		if(poprange<0.00025){return "1";}
-		else if ((poprange>=0.00025)&&(poprange<0.001)){return "2";}
-		else if ((poprange>=0.001)&&(poprange<0.005)){return "3";}
-		else if ((poprange>=0.005)&&(poprange<0.01)){return "4";}
-		else{return "5";}
+		if(pop.equals("null")){
+			return "1";
+		}
+		else{
+			Double poprange = Double.parseDouble(pop);
+			if(poprange<0.00025){return "1";}
+			else if ((poprange>=0.00025)&&(poprange<0.001)){return "2";}
+			else if ((poprange>=0.001)&&(poprange<0.005)){return "3";}
+			else if ((poprange>=0.005)&&(poprange<0.01)){return "4";}
+			else{return "5";}
+		}
 	}
 }
