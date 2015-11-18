28c1873ec8d3c314c24041a1b7b1a425528c0c16
takayabe0505 <yabe0505@iis.u-tokyo.ac.jp>
2015-11-18 10:32:48 +0900

    mlclean1020

--- src/MachineLearning/MLclean1020.java
+++ src/MachineLearning/MLclean1020.java
@@ -14,7 +14,7 @@ import java.util.Collections;
 public class MLclean1020 {
 
 	public static final String type      = "rain";
-	public static final String outdir    = "/home/c-tyabe/Data/MLResults_"+type+"9/";
+	public static final String outdir    = "/home/c-tyabe/Data/MLResults_"+type+"12/";
 	public static final String outdir2   = outdir+"forML/";
 	public static final String outdir3   = outdir+"forML/calc/";
 
@@ -49,6 +49,33 @@ public class MLclean1020 {
 
 
 	public static void naosu(String infile, String outfile) throws IOException{
+
+		BufferedReader br = new BufferedReader(new FileReader(new File(infile)));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(new File(outfile),true));
+		String line = null;
+
+		while((line=br.readLine())!=null){
+			String[] tokens = line.split(" ");
+			bw.write(tokens[0]);
+			ArrayList<Integer> list = new ArrayList<Integer>();
+			for(int i=1; i<tokens.length-1; i++){
+				if(!tokens[i].split(":")[0].isEmpty()){
+					Integer num = Integer.valueOf(tokens[i].split(":")[0]);
+					list.add(num);
+				}
+			}
+			Collections.sort(list);
+			for(Integer n : list){
+				bw.write(" "+String.valueOf(n)+":1");
+			}
+			bw.write(" "+tokens[tokens.length-1]);
+			bw.newLine();
+		}
+		br.close();
+		bw.close();
+	}
+	
+	public static void deletecomment(String infile, String outfile) throws IOException{
 
 		BufferedReader br = new BufferedReader(new FileReader(new File(infile)));
 		BufferedWriter bw = new BufferedWriter(new FileWriter(new File(outfile),true));
