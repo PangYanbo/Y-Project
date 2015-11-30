17e31262783d5b98305e448c5661ec6991d179f8
takayabe0505 <yabe0505@iis.u-tokyo.ac.jp>
2015-11-30 10:10:37 +0900

    updates

---
+++ src/MachineLearningAnalyse/Only1orMinus.java
@@ -0,0 +1,55 @@
+package MachineLearningAnalyse;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Only1orMinus {
+
+	public static void main(String args[]) throws IOException{
+
+		ArrayList<String> disasters = new ArrayList<String>();
+		disasters.add("rain");
+		disasters.add("eq");
+		disasters.add("heats");
+		disasters.add("dosha");
+
+		ArrayList<String> subjects = new ArrayList<String>();
+		subjects.add("home_exit");
+		subjects.add("tsukin_time");
+		subjects.add("office_enter");
+		subjects.add("office_time");
+		subjects.add("office_exit");
+		subjects.add("kitaku_time");
+		subjects.add("home_return");
+
+		for(String disaster : disasters){
+			for(String subject : subjects){
+				String in = "/home/c-tyabe/Data/MLResults_"+disaster+"12/forML/calc/"+subject+"_diff_ML2_plusminus_lineforeach.csv";
+				String out = "/home/c-tyabe/Data/MLResults_"+disaster+"12/forML/calc/"+subject+"_diff_ML2_plusminus_lineforeach_nozero.csv";
+				removezero(in,out);
+			}
+		}
+	}
+
+	public static void removezero(String in, String out) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(new File(in)));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(new File(out)));
+		String line = null;
+		while((line = br.readLine())!= null){
+			String[] tokens = line.split(" ");
+			String date = tokens[0];
+			if(!date.equals("0")){
+				bw.write(line);
+				bw.newLine();
+			}
+		}
+		br.close();
+		bw.close();
+	}
+
+}
---
+++ src/MachineLearningAnalyse/SoseiAnalyser.java
@@ -0,0 +1,121 @@
+package MachineLearningAnalyse;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class SoseiAnalyser {
+
+	public static void main(String args[]) throws IOException{
+
+		/**@author yabetaka
+		 */
+
+		String type = "rain";
+		Integer n = 100;
+		String para = "0.01";
+
+		File soseifile = new File("c:/users/yabetaka/desktop/Yahoo/sosei_bilinearincluded.txt");
+		HashMap<String,String> map = getsoseimap(soseifile);
+
+		ArrayList<String> subjects = new ArrayList<String>();
+		subjects.add("home_exit");
+		subjects.add("tsukin_time");
+		subjects.add("office_enter");
+		subjects.add("office_time");
+		subjects.add("office_exit");
+		subjects.add("kitaku_time");
+		subjects.add("home_return");
+
+		for(String s:subjects){
+			checkfile(type,s,para,n,map);
+		}
+
+	}
+
+	public static void checkfile(String type, String subject, String para, Integer n, HashMap<String,String> map) throws IOException{
+		File in = new File("C:/Users/yabetaka/Desktop/Yahoo/soseiresults/"+type+"sosei/"+subject+"_diff_ML2_plusminus_lineforeach_word_c_"+para+"v_-1");
+		BufferedReader br = new BufferedReader(new FileReader(in));
+		String line = null;
+		HashMap<String, Double> list = new HashMap<String,Double>();
+		while((line=br.readLine())!=null){
+			String[] tokens = line.split(",");
+			String num  = tokens[0];
+			//			String desc = tokens[1];
+			String[] nums = tokens[2].split(" ");
+			Double one   = Double.parseDouble(nums[0]);
+			//			Double zero  = Double.parseDouble(nums[1]);
+			Double minus = Double.parseDouble(nums[2]);
+			Double sum   = Math.abs(one)+Math.abs(minus);
+			list.put(num, sum);
+		}
+		br.close();
+
+		System.out.println(" ");
+		System.out.println(type+","+subject);
+		sortandshow(list,n,map);
+	}
+
+	public static void sortandshow(HashMap<String,Double> map, Integer n, HashMap<String,String> soseimap){
+		List<Map.Entry<String,Double>> entries = 
+				new ArrayList<Map.Entry<String,Double>>(map.entrySet());
+		Collections.sort(entries, new Comparator<Map.Entry<String,Double>>() {
+
+			@Override
+			public int compare(Entry<String,Double> entry1, Entry<String,Double> entry2) {
+				return ((Double)entry2.getValue()).compareTo((Double)entry1.getValue());
+			}
+		});
+
+		ArrayList<String> temp = new ArrayList<String>();
+		int count = 0;
+		for (Entry<String,Double> s : entries) {
+			String r = numchanger(s.getKey());
+			if(!temp.contains(r)){
+				if((Integer.valueOf(s.getKey())<1000)||(Integer.valueOf(s.getKey())>=100000)){
+					System.out.println(soseimap.get(s.getKey())+","+s.getKey()+","+s.getValue());
+					count++;
+					temp.add(r);
+					if(count==n){
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	public static HashMap<String,String> getsoseimap(File in) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(in));
+		String line = null;
+		HashMap<String, String> list = new HashMap<String,String>();
+		while((line=br.readLine())!=null){
+			String[] tokens = line.split(" ");
+			String num = tokens[0];
+			String des = tokens[1];
+			list.put(num, des);
+		}
+		br.close();
+		return list;
+	}
+
+	public static String numchanger(String num){
+		Integer n = Integer.valueOf(num);
+		if(n<=4){
+			String range = "0";
+			return range;
+		}
+		else{
+			String range = String.valueOf((int)(n-5)/5);
+			return range;
+		}
+	}
+
+}
