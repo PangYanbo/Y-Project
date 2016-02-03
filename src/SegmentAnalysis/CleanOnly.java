6a2122a9749233ef98454785dda826650d483e6f
takayabe0505 <yabe0505@iis.u-tokyo.ac.jp>
2016-02-03 18:07:23 +0900

    oh baby

---
+++ src/SegmentAnalysis/CleanOnly.java
@@ -0,0 +1,20 @@
+package SegmentAnalysis;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CleanOnly {
+
+	private static final String homepath = "c:/Users/t-tyabe/Desktop/";
+
+	public static void main(String args[]) throws IOException{
+		
+		String type = "rain";
+		
+		File res = new File(homepath+type+"distance_temp.csv");
+		File cleanres = new File(homepath+type+"distance_clean.csv");
+		TotalMovementLength.clean(res,cleanres);
+
+	}
+	
+}
