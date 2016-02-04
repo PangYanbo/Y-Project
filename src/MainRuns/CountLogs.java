ac9c3be17f0cc1fe7d0d2556cddd87805b4e4512
takayabe0505 <yabe0505@iis.u-tokyo.ac.jp>
2016-02-04 10:51:23 +0900

    updaupte

--- src/MainRuns/CountLogs.java
+++ src/MainRuns/CountLogs.java
@@ -15,7 +15,7 @@ public class CountLogs {
 		while((line=br.readLine())!=null){
 			String[] tokens = line.split("\t");
 			String id = tokens[0];
-			String date = tokens[3]; //yyyy-mm-ddTHH:MM:SS:ss 
+			String date = tokens[3]; //yyyy-mm-dd HH:MM:SS
 			String yyyymmdd = getonlydate(date);
 			if(!yyyymmdd.equals(disdate)){
 				if(res.containsKey(id)){
@@ -38,7 +38,7 @@ public class CountLogs {
 		while((line=br.readLine())!=null){
 			String[] tokens = line.split("\t");
 			String id = tokens[0];
-			String date = tokens[3]; //yyyy-mm-ddTHH:MM:SS:ss 
+			String date = tokens[3]; //yyyy-mm-dd HH:MM:SS
 			String yyyymmdd = getonlydate(date);
 			if(yyyymmdd.equals(disdate)){
 				if(res.containsKey(id)){
@@ -55,7 +55,7 @@ public class CountLogs {
 	}
 	
 	public static String getonlydate(String t){
-		String[] x = t.split("T");
+		String[] x = t.split(" ");
 		String res = x[0];
 		String date = res.split("-")[0]+res.split("-")[1]+res.split("-")[2];
 		return date;
